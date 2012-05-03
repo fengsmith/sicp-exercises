563f5effcd2611d50e804a839ebe3c834dbb4c45
Francisco Soto <ebobby@gmail.com>
2012-05-03 01:59:49 -0700

    Exercise 1.7

--- chapter-1/1.7.scm
+++ chapter-1/1.7.scm
@@ -5,10 +5,11 @@
 ;; stop when the change is a very small fraction of the guess. Design a square-root procedure that uses this kind of end test. Does
 ;; this work better for small and large numbers?
 
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+(define (sqrt-iter guess previous-guess x)
+  (if (good-enough? guess previous-guess x)
       guess
       (sqrt-iter (improve guess x)
+                 guess
                  x)))
 
 (define (improve guess x)
@@ -17,8 +18,8 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+(define (good-enough? guess previous-guess x)
+  (< (abs (- guess previous-guess)) 0.001))
 
 (define (sqrt x)
-  (sqrt-iter 1.0 x))
+  (sqrt-iter 1.0 0.0  x))
